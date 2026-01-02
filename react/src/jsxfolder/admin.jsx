8e097b70289b8868340e85a7aa371ef28aafaccf
Akashhogetapal <akash85486@gmail.com>
2026-01-02 22:34:05 +0530

    admin

--- react/src/jsxfolder/admin.jsx
+++ react/src/jsxfolder/admin.jsx
@@ -2,8 +2,21 @@ import "../css/admin.css"
 
 function Admin() {
     return (
-        <div className="admin-page">
-            <h1 className="header">Admin Page</h1>
+        <div className="cont">
+            <header>
+            <div className="admin-header">
+                <h1>adm<span className="red-i">I</span>n dashboard</h1>
+            </div>
+            </header>
+          <div className="req">
+            <p className="incom-para">Incomiung Reaquests</p>
+          </div>
+
+
+
+
+
+
         </div>
     );
 }
