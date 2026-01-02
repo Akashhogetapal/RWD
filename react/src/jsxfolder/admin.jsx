99a0225b28bbd03aaf4aafe073abfc29e2b401ac
Akashhogetapal <akash85486@gmail.com>
2026-01-03 01:12:39 +0530

    today admin

--- react/src/jsxfolder/admin.jsx
+++ react/src/jsxfolder/admin.jsx
@@ -8,7 +8,7 @@ function Admin() {
                 <h1>adm<span className="red-i">I</span>n dashboard</h1>
             </div>
             </header>
-          
+          <div className="req-act">
           <div className="req">
             <p className="incom-para">Incomiung Reaquests</p>
           <div className="main-req-row">
@@ -19,12 +19,50 @@ function Admin() {
                <span>UTR</span>
                <span>ACTION</span>
             </div>
-
+            
+            <div className="req-row">
+                <span className="user-cell">
+                  <span className="red-circle"></span>
+                       John Doe
+                     </span>
+                <span>12345</span>
+                <span>500</span>
+                <span>12345678900</span>
+               <div className="action-buttons">
+                   <button className="btn reject">✕</button>
+                    <button className="btn accept">✓</button>
+                </div>
+            </div>
           </div>
          </div>
 
+         <div className="activity">
+            <p className="incom-para">Reacent Activity</p>
+          <div className="main-req-row">
+            <div className="req-row">
+               <span>USER</span>
+               <span>AMOUNT</span>
+               <span>STATUS</span>
+               <span>DATE</span>
+               <span>TIME</span>
+            </div>
+            
+            <div className="req-row">
+                <span className="user-cell">
+                  <span className="red-circle"></span>
+                       John Doe
+                     </span>
+                <span>123</span>
+                <span className="rejected1">
+                <span className="rejected">✕ Rejected</span> 
+                </span>
+                <span>21/12/2000</span>
+                <span>12:00 PM</span>
 
-
+            </div>
+          </div>
+         </div>
+        </div>
 
 
         </div>
