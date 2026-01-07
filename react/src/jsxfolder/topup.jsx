391ac1d6b4ff35a794dc4a8acea270df793f2b16
Akashhogetapal <akash85486@gmail.com>
2026-01-07 20:23:22 +0530

    api fixed

--- react/src/jsxfolder/topup.jsx
+++ react/src/jsxfolder/topup.jsx
@@ -19,7 +19,7 @@ function Topup() {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
-          key: localStorage.getItem("userKey") // IMPORTANT
+          key: localStorage.getItem("userKey") 
         })
       });
 
