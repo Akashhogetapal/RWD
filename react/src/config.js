fe6d24e52d230f6510a9d322734dd7b53b8e1e99
Akashhogetapal <akash85486@gmail.com>
2026-07-27 17:36:50 +0530

    api

--- react/src/config.js
+++ react/src/config.js
@@ -1,2 +1,3 @@
 // export const API_BASE_URL = "http://localhost:8080";
-export const API_BASE_URL = "https://rwd.up.railway.app";
+// export const API_BASE_URL = "https://rwd.up.railway.app";
+export const API_BASE_URL = "http://rwd.railway.internal";
